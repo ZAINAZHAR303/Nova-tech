58fd283557e451ebf462240c90e0fac3bed8844b
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2024-11-25 18:02:50 +0500

    update firebase authorization

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,13 @@
 import Image from "next/image";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-<<<<<<< HEAD:src/app/page.jsx
       {/* <h2 className="text-4xl">zubair kaala</h2> */}
       <Navbar />
-=======
-      <h1 className="text-6xl">Zain moota</h1>
-      <h2 className="text-4xl">zubair kaala</h2>
->>>>>>> a072151abb7dcc7df99d55e99baaf5e2ff0a7303:src/app/page.js
+
+  
     </div>
   );
 }
